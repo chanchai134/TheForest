0f5f82b35959253940dc8d05942f661c8071b82a
Chanchai <pom51142@gmail.com>
2017-10-03 19:05:26 +0700

    add physic

--- model_theforest.py
+++ model_theforest.py
@@ -7,17 +7,23 @@ class Model_F:
         self.y = y
 
 class Shoot_F(Model_F):
-    def __init__(self, width, height, x, y, accelerate, velocity, gravity, angle):
+    def __init__(self, width, height, x, y, velocity, gravity, angle):
         super().__init__(width, height, x, y)
-        self.accelerate = accelerate
-        self.velocity = velocity
-        self.gravity = gravity
-        self.angle = angle
+        self.gravity = (-1)*gravity
+        self.angle = (angle*math.pi)/180
+        self.velocity_x = velocity*math.cos(self.angle)
+        self.velocity_y_pre = velocity*math.sin(self.angle)
+        self.velocity_y_post = velocity*math.sin(self.angle) + self.gravity
     def shoot(self):
-        self.x += 1
+        shiff_distance_y = math.pow(self.velocity_y_post,2)-math.pow(self.velocity_y_pre,2)
+        shiff_distance_y /= 2*self.gravity
+        self.velocity_y_pre += self.gravity
+        self.velocity_y_post += self.gravity
+        self.x += self.velocity_x
+        self.y += shiff_distance_y
 
 class Grape_F(Shoot_F):
-    def __init__(self, width, height, x, y, accelerate, velocity, gravity, angle):
-        super().__init__(width, height, x, y, accelerate, velocity, gravity, angle)
+    def __init__(self, width, height, x, y, velocity, gravity, angle):
+        super().__init__(width, height, x, y, velocity, gravity, angle)
 
         
--- theforest.py
+++ theforest.py
@@ -4,17 +4,19 @@ import sprite_theforest
 
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 600
+GRAVITY = 0.05
 
 class GameWindow(arcade.Window):
     def __init__(self, width, height,title):
         super().__init__(width, height,title)
         arcade.set_background_color(arcade.color.BLACK)
-    grape_setting = model_theforest.Grape_F(68,91,100,300,20,10,10,30)
+    grape_setting = model_theforest.Grape_F(68,91,100,300,6,GRAVITY,45)
     grape_sprite = sprite_theforest.Sprite_F("images/grape.png",grape_setting)
     def on_draw(self):
         arcade.start_render()
         self.grape_sprite.draw()
     def update(self, delta):
+        self.grape_setting.shoot()
         self.grape_sprite.update()
 
 if __name__ == '__main__':
