aea5a5c7ca041eb52983ae128cd4d6d04b55d9db
Chanchai <pom51142@gmail.com>
2017-10-05 22:45:10 +0700

    add image file

---
+++ setting_theforest.py
@@ -0,0 +1,11 @@
+class Mouse_C:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.aimming = False
+        self.shooting = False
+        self.canCaught = False
+    def reset_mouse(self):
+        self.aimming = False
+        self.shooting = False
+        self.canCaught = False
